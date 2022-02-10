d3aa4a31bc677d3cf12dcb11ff0bcb6d7df71473
agueroveraalvaro <aguero.vera.alvaro@gmail.com>
2022-02-10 11:01:49 -0300

    Initialized project

---
+++ lib/get_better_resolution.dart
@@ -0,0 +1,240 @@
+library get_better_resolution;
+
+/*
+NanoBanco Mobile App - CryptoMarket
+
+File edited by: Álvaro Agüero - 4/2/2022
+*/
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'multi_width_height_image.dart';
+import 'package:flutter/foundation.dart';
+
+class GetBetterResolution {
+
+  static Map<String, MultiWidthHeightImage> imagesMap = {};
+
+  Future<void> initialize({
+    required String folderInitRoute
+  }) async {
+    if (kDebugMode) {
+      print('<< GetBetterResolution ---- initialize >>');
+      print(folderInitRoute);
+    }
+
+    imagesMap = {};
+
+    final _manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> _manifestMap = json.decode(_manifestContent);
+
+    final _imagePaths = _manifestMap.keys
+        .where((String key) => key.contains(folderInitRoute))
+        .where((String key) => key.contains('.png'))
+        .toList();
+
+    for (var originalElement in _imagePaths) {
+
+      final _originalImageNameArray = originalElement.split('/');
+      final _originalImageName = _originalImageNameArray[_originalImageNameArray.length-1];
+
+      final _originalImageNameDataArray = _originalImageName.split('.');
+      final _originalImageNameExtracted = _originalImageNameDataArray[1];
+      final _originalReferencePath = folderInitRoute + _originalImageNameExtracted;
+
+      if(imagesMap.containsKey(_originalReferencePath)) {
+
+        final _imageNameArray = originalElement.split('/');
+        final _imageName = _imageNameArray[_imageNameArray.length-1];
+
+        final _imageNameDataArray = _imageName.split('.');
+        final imageNameExtracted = _imageNameDataArray[1];
+
+        if(_originalImageNameExtracted == imageNameExtracted) {
+          final _imageSizeExtracted = _imageNameDataArray[0];
+          final _sizeImageExtractedArray = _imageSizeExtracted.split('x');
+          final _width = int.tryParse(_sizeImageExtractedArray[0]) ?? 100;
+          final _height = int.tryParse(_sizeImageExtractedArray[1]) ?? 100;
+
+          imagesMap[_originalReferencePath]!.widths.add(_width);
+          imagesMap[_originalReferencePath]!.heights.add(_height);
+          imagesMap[_originalReferencePath]!.paths.add(originalElement);
+        }
+      } else {
+        final _originalImageSizeExtracted = _originalImageNameDataArray[0];
+        final _sizeImageExtractedArray = _originalImageSizeExtracted.split('x');
+        final _originalWidth = int.tryParse(_sizeImageExtractedArray[0]) ?? 100;
+        final _originalHeight = int.tryParse(_sizeImageExtractedArray[1]) ?? 100;
+
+        imagesMap[_originalReferencePath] = MultiWidthHeightImage(
+            widths: [_originalWidth],
+            heights: [_originalHeight],
+            paths: [originalElement]
+        );
+      }
+    }
+
+    //Order data
+    imagesMap.forEach((key, value) {
+      for(int b=0; b<value.paths.length; b++) {
+
+        final bTotalResolution = value.widths[b] + value.heights[b];
+
+        for(int c=0; c<value.paths.length; c++) {
+
+          final cTotalResolution = value.widths[c] + value.heights[c];
+
+          if(cTotalResolution > bTotalResolution) {
+
+            final String pathsAux = value.paths[b];
+            final int widthAux = value.widths[b];
+            final int heightAux = value.heights[b];
+
+            value.paths[b] = value.paths[c];
+            value.widths[b] = value.widths[c];
+            value.heights[b] = value.heights[c];
+
+            value.paths[c] = pathsAux;
+            value.widths[c] = widthAux;
+            value.heights[c] = heightAux;
+          }
+        }
+      }
+    });
+  }
+
+  String get({
+    required BuildContext context,
+    required String referencePath,
+    int? width,
+    int? height
+  }) {
+
+    if (kDebugMode) {
+      print('===================== imagesMap get() ======================');
+      imagesMap.forEach((key, value) {
+        if (kDebugMode) {
+          print('key $key - value $value');
+        }
+        if(key == 'assets/images/icons/resolution_icons/balance') {
+          for(int b=0; b<value.paths.length; b++) {
+            if (kDebugMode) {
+              print('heights $b - ' + value.heights[b].toString()
+                + '  widths $b - ' + value.widths[b].toString());
+            }
+          }
+        }
+      });
+    }
+
+    if(width == null && height == null) {
+      return 'Debe venir al menos un valor válido para width o height.';
+    }
+
+    if(imagesMap.containsKey(referencePath)) {
+
+      final MultiWidthHeightImage multiWidthHeightImage = imagesMap[referencePath]!;
+
+      //Work with [width]
+      if(width != null) {
+
+        final baseWidth = multiWidthHeightImage.widths[0];
+        final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+        final widthCalculated = (baseWidth * devicePixelRatio * (width / baseWidth)).round();
+
+        List<List<int>> positionAndDifferenceList = [];
+
+        if(widthCalculated < baseWidth) {
+          return multiWidthHeightImage.paths[0];
+        }
+
+        for(int b=0; b<multiWidthHeightImage.paths.length; b++) {
+
+          if(widthCalculated == multiWidthHeightImage.widths[b]) {
+            return multiWidthHeightImage.paths[b];
+          }
+
+          positionAndDifferenceList.add(
+              [
+                b,//[0] position
+                (widthCalculated - multiWidthHeightImage.widths[b]).round().abs() //[1] difference with widthCalculated, here is rounded and positive
+              ]
+          );
+        }
+
+        for(int b=0; b<positionAndDifferenceList.length; b++) {
+          for(int c=0; c<positionAndDifferenceList.length; c++) {
+
+            if(positionAndDifferenceList[c][1] > positionAndDifferenceList[b][1]) {
+
+              final positionAndDifferenceAux = positionAndDifferenceList[b];
+              positionAndDifferenceList[b] = positionAndDifferenceList[c];
+              positionAndDifferenceList[c] = positionAndDifferenceAux;
+
+            }
+          }
+        }
+
+        if(multiWidthHeightImage.paths.length > 1) {
+          //The nearest is the last
+          if(positionAndDifferenceList[0][0] == multiWidthHeightImage.paths.length-1) {
+            return multiWidthHeightImage.paths[positionAndDifferenceList[0][0]];
+          } else
+          if(widthCalculated > multiWidthHeightImage.widths[positionAndDifferenceList[0][0]]) {
+            return multiWidthHeightImage.paths[positionAndDifferenceList[1][0]];
+          }
+        }
+        return multiWidthHeightImage.paths[positionAndDifferenceList[0][0]];
+      } else { //Work with [height]
+
+        final baseHeight = multiWidthHeightImage.heights[0];
+        final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+        final heightCalculated = (baseHeight * devicePixelRatio * (height! / baseHeight)).round();
+
+        List<List<int>> positionAndDifferenceList = [];
+
+        if(heightCalculated < baseHeight) {
+          return multiWidthHeightImage.paths[0];
+        }
+
+        for(int b=0; b<multiWidthHeightImage.paths.length; b++) {
+
+          if(heightCalculated == multiWidthHeightImage.heights[b]) {
+            return multiWidthHeightImage.paths[b];
+          }
+
+          positionAndDifferenceList.add(
+              [
+                b,//[0] position
+                (heightCalculated - multiWidthHeightImage.heights[b]).round().abs() //[1] difference with heightCalculated, here is rounded and positive
+              ]
+          );
+        }
+
+        for(int b=0; b<positionAndDifferenceList.length; b++) {
+          for(int c=0; c<positionAndDifferenceList.length; c++) {
+            if(positionAndDifferenceList[c][1] > positionAndDifferenceList[b][1]) {
+              final positionAndDifferenceAux = positionAndDifferenceList[b];
+              positionAndDifferenceList[b] = positionAndDifferenceList[c];
+              positionAndDifferenceList[c] = positionAndDifferenceAux;
+            }
+          }
+        }
+
+        if(multiWidthHeightImage.paths.length > 1) {
+          //The nearest is the last
+          if(positionAndDifferenceList[0][0] == multiWidthHeightImage.paths.length-1) {
+            return multiWidthHeightImage.paths[positionAndDifferenceList[0][0]];
+          } else
+          if(heightCalculated > multiWidthHeightImage.heights[positionAndDifferenceList[0][0]]) {
+            return multiWidthHeightImage.paths[positionAndDifferenceList[1][0]];
+          }
+        }
+        return multiWidthHeightImage.paths[positionAndDifferenceList[0][0]];
+      }
+    } else {
+      return 'El path no está con el formato correcto.';
+    }
+  }
+}
---
+++ lib/multi_width_height_image.dart
@@ -0,0 +1,18 @@
+/*
+NanoBanco Mobile App - CryptoMarket
+
+File edited by: Álvaro Agüero - 4/2/2022
+*/
+
+class MultiWidthHeightImage {
+
+  List<int> widths;
+  List<int> heights;
+  List<String> paths;
+
+  MultiWidthHeightImage({
+    required this.widths,
+    required this.heights,
+    required this.paths
+  });
+}
