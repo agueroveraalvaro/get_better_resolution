61f55e3167edd03aa4e1f7088964e88c87440d2f
agueroveraalvaro <aguero.vera.alvaro@gmail.com>
2022-03-15 13:42:17 -0300

    + getSync added

---
+++ lib/resolution_aware_image_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ResolutionAwareImageWidget extends StatefulWidget {
+
+  const ResolutionAwareImageWidget({
+    Key? key
+  }) : super(key: key);
+
+  @override
+  _ResolutionAwareImageWidgetState createState() => _ResolutionAwareImageWidgetState();
+}
+
+class _ResolutionAwareImageWidgetState extends State<ResolutionAwareImageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
